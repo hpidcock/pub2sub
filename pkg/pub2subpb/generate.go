f1d6e182a0cf214f4ae0c809e98e02a432f2df3c
Harry Pidcock <haza55@gmail.com>
2018-06-08 00:25:28 +1000

    Update deps

--- pkg/pub2subpb/generate.go
+++ pkg/pub2subpb/generate.go
@@ -1,3 +1,3 @@
 package pub2subpb
 
-//go:generate protoc -I ../../protobuf ../../protobuf/pub2sub/pub2sub.proto --go_out=plugins=grpc:../../../../../ --twirp_out=../../../../../
+//go:generate protoc -I ../../protobuf ../../protobuf/pub2sub/pub2sub.proto --go_out=plugins=grpc:../../../../../
